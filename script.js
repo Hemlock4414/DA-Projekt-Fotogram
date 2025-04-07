44d8e3af4a2fe51a1b99eb20e1b40bc495af33eb
Hemlock4414 <antonio.marrara@gmx.ch>
2025-04-07 12:37:58 +0200

    funktioniert

--- script.js
+++ script.js
@@ -1,48 +1,108 @@
-let images = [
-    "./img/amphibian_axolotl_1280.jpg",
+let imagesAll = [
+    "./img/reptile_snake_1280.jpg",
+    "./img/bird_parrot_1280.jpg",
     "./img/amphibian_frog_1280.jpg",
+    "./img/mammal_elephant_1280.jpg",
     "./img/amphibian_salamander_1280.jpg",
+    "./img/reptile_lizard_1280.jpg",
     "./img/amphibian_toad_1280.jpg",
     "./img/bird_eagle_1280.jpg",
-    "./img/bird_owl_1280.jpg",
-    "./img/bird_parrot_1280.jpg",
-    "./img/bird_pinguin_1280.jpg",
     "./img/mammal_cat_1280.jpg",
-    "./img/mammal_dolphin_1280.jpg",
-    "./img/mammal_elephant_1280.jpg",
+    "./img/bird_owl_1280.jpg",
     "./img/mammal_lion_1280.jpg",
-    "./img/reptile_crocodile_1280.jpg",
-    "./img/reptile_lizard_1280.jpg",
-    "./img/reptile_snake_1280.jpg",
+    "./img/bird_pinguin_1280.jpg",
+    "./img/mammal_dolphin_1280.jpg",
+    "./img/reptile_crocodile_1280.jpg", 
+    "./img/amphibian_axolotl_1280.jpg",
     "./img/reptile_tortoise_1280.jpg"
 ];
+
+let imagesMammals = [
+  "./img/mammal_elephant_1280.jpg",
+  "./img/mammal_cat_1280.jpg",
+  "./img/mammal_lion_1280.jpg",
+  "./img/mammal_dolphin_1280.jpg",
+];
+
+let imagesBirds = [
+  "./img/bird_parrot_1280.jpg",
+  "./img/bird_eagle_1280.jpg",
+  "./img/bird_owl_1280.jpg",
+  "./img/bird_pinguin_1280.jpg",
+];
+
+let imagesReptiles = [
+  "./img/reptile_snake_1280.jpg",
+  "./img/reptile_lizard_1280.jpg",
+  "./img/reptile_crocodile_1280.jpg", 
+  "./img/reptile_tortoise_1280.jpg"
+];
+
+let imagesAmphibians = [
+  "./img/amphibian_frog_1280.jpg",
+  "./img/amphibian_salamander_1280.jpg",
+  "./img/amphibian_toad_1280.jpg",
+  "./img/amphibian_axolotl_1280.jpg",
+];
+
+let currentImages = [];
+let currentDescriptions = [];
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("default-open").click();
 
-function openAnimals(event, animalName) {  
-    // Get all elements with class="tabcontent" and hide them
-    let tabcontent = document.getElementsByClassName("tabcontent");
-    for (let i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
+function renderFiltered(event, index) {
+
+  // Setze alle Buttons zurück und aktiviere den aktuellen
     let tablinks = document.getElementsByClassName("tablinks");
     for (let i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(animalName).style.display = "flex";
-    document.getElementById(animalName).classList.add("active");
     event.currentTarget.className += " active";
+
+  if(index == 'show-all') {
+    currentImages = imagesAll;
+  }
+  else if(index == 'mammals') {
+    currentImages = imagesMammals;
+  }
+  else if(index == 'birds') {
+    currentImages = imagesBirds;
+  }
+  else if(index == 'reptiles') {
+    currentImages = imagesReptiles;
+  }
+  else if(index == 'amphibians') {
+    currentImages = imagesAmphibians;
+  }
+  renderImages();
 }
 
 function renderImages() {
-  let imageContainer = document.getElementById("show-all");
-  imageContainer.innerHTML = "";
-  for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
-      imageContainer.innerHTML += `<img onclick="openImage(${imageIndex})" src="${images[imageIndex]}">`;
+  let imageTabContent = document.getElementById('tabcontent');
+  imageTabContent.innerHTML = "";
+
+    // Aktiviere die Anzeige des Content-Bereichs
+    imageTabContent.style.display = "flex";
+    imageTabContent.classList.add("active");
+
+  for (let imageIndex = 0; imageIndex < currentImages.length; imageIndex++) {
+    imageTabContent.innerHTML += createImageTemplate(imageIndex);
   }
 }
+
+function createImageTemplate(imageIndex) {
+  return  `<img onclick="openImage(${imageIndex})" src="${currentImages[imageIndex]}">`;
+}
+
+// function renderImagesOverlay() {
+//   let contentRef = document.getElementById('tabcontent');
+//   contentRef.innerHTML = "";
+//   for (let index = 0 ; index < currentDescriptions.length; index++) {
+//     contentRef.innerHTML += getNoteTemplate(index, currentDescriptions, currentImages);
+//   }
+// }
+
+
+
+
